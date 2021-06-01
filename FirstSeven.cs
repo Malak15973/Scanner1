7c491ffb9378ecbd7451309575dd02c367aa3ff0
Shady <ShadyZekry75@gmail.com>
2021-06-01 19:03:04 +0200

    Add character type to scanner "Chji"

--- FirstSeven.cs
+++ FirstSeven.cs
@@ -77,5 +77,41 @@ namespace Scanner1
             if (state == 10) Console.WriteLine("Matched\tSIMww");
             else Console.WriteLine("Not Matched");
         }
+        
+        //Chji
+        public void characterType(string Lexeme)
+        {
+            int state = 1, i = 0;
+            char character;
+            while (state != 8 && state != 0)
+            {
+                character = Lexeme[i];
+                switch (state)
+                {
+                    case 1:
+                        if (character == 'C') state = 2;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 2:
+                        if (character == 'h') state = 4;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 4:
+                        if (character == 'j') state = 6;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 6:
+                        if (character == 'i') state = 8;
+                        else state = 0;
+                        i++;
+                        break;
+                }
+            }
+            if (state == 8) Console.WriteLine("Matched\tChji");
+            else Console.WriteLine("Not Matched");
+        }
     }
 }
