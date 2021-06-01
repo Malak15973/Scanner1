bed666a23a49aad28adbf4c36b9c6ef638f9a17e
Shady <ShadyZekry75@gmail.com>
2021-06-01 18:57:45 +0200

    Add signed integer type to scanner "SIMww"

--- FirstSeven.cs
+++ FirstSeven.cs
@@ -36,5 +36,46 @@ namespace Scanner1
             if (state == 8) Console.WriteLine("Matched\tOmw");
             else Console.WriteLine("Not Matched");
         }
+
+        //SIMww
+        public void signedIntegerType(string Lexeme)
+        {
+            int state = 1, i = 0;
+            char character;
+            while (state != 10 && state != 0)
+            {
+                character = Lexeme[i];
+                switch (state)
+                {
+                    case 1:
+                        if (character == 'S') state = 2;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 2:
+                        if (character == 'I') state = 4;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 4:
+                        if (character == 'M') state = 6;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 6:
+                        if (character == 'w') state = 8;
+                        else state = 0;
+                        i++;
+                        break;
+                    case 8:
+                        if (character == 'w') state = 10;
+                        else state = 0;
+                        i++;
+                        break;
+                }
+            }
+            if (state == 10) Console.WriteLine("Matched\tSIMww");
+            else Console.WriteLine("Not Matched");
+        }
     }
 }
