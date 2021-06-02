3d1afac4fbf6c0aff65da10a5de9d4d833b30859
Seif_Tahawy <seiftahawy54@gmail.com>
2021-06-02 06:55:50 +0200

    First Version Of the Stack

---
+++ CustomStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace full_stack
+{
+    class CustomStack <T>
+    {
+        private T [] _ps = {};
+        private int _size = 0;
+
+        public void Push(T item)
+        {
+            this._ps[_size+1] = item;
+            this._size += 1;
+        }
+
+        public Boolean Pop(T item)
+        {
+            if (_size == 0)
+            {
+                return false;
+            }
+            else
+            {
+                this._size -= 1;
+                return true;
+            }
+        }
+
+        public int Search(T item)
+        {
+            for (int i = 0; i < this._size; i++)
+            {
+                if (this._ps[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int StackSize()
+        {
+            return this._size;
+        }
+
+        public void TraverseStack(Action<T> funct)
+        {
+            for (int i = 0; i < this._size; i++)
+            {
+                funct(this._ps[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace full_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<char> st = new CustomStack<char>();
+
+
+            st.Push('c');
+
+            st.TraverseStack(Console.WriteLine);
+        }
+    }
+}
