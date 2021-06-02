fc2811ea15a0b8f5b9df7045625c02601d19e5b6
Shady <ShadyZekry75@gmail.com>
2021-06-02 23:37:50 +0200

    Add code for indentifiers tokenization

---
+++ Find.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanner1
+{
+    class Find : IRepositoryFindComment
+    {
+        int errorsNumber = 0;
+        int lineNumber = 3;
+        int lexemeNumber = 3;
+        int index = 0;
+        string code = "$dsfjjkl(";
+
+        public void FindIdentifiers()
+        {
+            index++; // to skip the $
+            String identifier = "";
+            while (!isSpecialChar())
+            {
+                index++;
+                identifier += code[index];
+            }
+            
+            if (identifier == "")  // If nothing is valid after the $
+            {
+                lexemeNumber++;
+                Console.WriteLine(lineNumber + "\t" + identifier + "\tIdentifier\t" + lexemeNumber + "\tUnmatched");
+                return;
+            }
+
+            lexemeNumber++;
+            Console.WriteLine(lineNumber + "\t" + identifier + "\tIdentifier\t" + lexemeNumber + "\tmatched");
+
+            if (code[index] == ",")
+                FindIdentifiers(); // Recursion
+        }
+
+    }
+}
