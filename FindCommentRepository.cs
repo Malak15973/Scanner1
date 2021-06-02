d2e52a155e3cc690515e983e4a417cee04cf2cf4
fadyfayek11 <fayekfady24@gmail.com>
2021-06-02 22:11:09 +0200

    Find Singel or Multi Comments

---
+++ FindCommentRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanner1
+{
+    class FindCommentRepository : IRepositoryFindComment
+    {
+        int index = 0;
+        string Lexem = "/^adsada\ndsdsadsa";
+        public int FindMultipleLinesComment()
+        {
+            char c;
+            int length = Lexem.Length;
+            for (int i = 0; i < length; i++)
+            {
+                c = Lexem[i];
+                if (c == '/')
+                {
+                    c = Lexem[++i];
+                    if (c == '@')
+                    {
+                        for (int j = i; j < length; j++)
+                        {
+                            c = Lexem[j];
+                            if (c == '@')
+                            {
+                                c = Lexem[++j];
+                                if (c == '/')
+                                {
+                                    return index = j + 1;
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+            return index;
+
+        }       
+
+        public int FindSingleLineComment()
+        {
+            char c;
+            int length = Lexem.Length;
+            for (int i = 0; i < length; i++)
+            {
+                c = Lexem[i];
+                if (c == '/')
+                {
+                    c = Lexem[++i];
+                    if (c == '^')
+                    {
+                        for (int j = i; j < length; j++)
+                        {
+                            c = Lexem[j];
+                            if (c == '\n')
+                            {
+
+                                return index = j + 1;
+
+                            }
+
+                        }
+                    }
+                }
+            }
+            return index;
+
+        }
+    }
+}
---
+++ IRepositoryFindComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanner1
+{
+    public interface IRepositoryFindComment
+    {
+        public int FindMultipleLinesComment();
+        public int FindSingleLineComment();
+
+    }
+}
